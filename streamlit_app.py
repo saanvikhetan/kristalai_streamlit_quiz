29f2cb6a8b64ac0fe47a2c53ecd5e7eb0beffb5e
saanvikhetan <saanvikhetan@gmail.com>
2024-07-07 21:23:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,40 @@
 import streamlit as st
 
+topics_list = ["Securities", "Securities-based derivatives contract", "Securities Industry Council"]
+
+if "show_topic_choice" not in st.session_state:
+    st.session_state.show_topic_choice = True
+
+
 st.title("Quiz")
 st.write(
     "Securities and Futures Act 2001"
 )
 
+
+
 def quiz():
     st.write("Starting quiz...")
+    st.session_state.show_topic_choice = False
+    
     
 
-topics_list = ["Securities", "Securities-based derivatives contract", "Securities Industry Council"]
-
-options = st.multiselect(
-    "What topics would you like to be quizzed on?",
-    (topics_list)
-)
-
-topics_selected = []
-for x in topics_list:
-    if x in options:
-        topics_selected.append(topics_list.index(x))
-
-
-st.write("You selected:", options)
-st.write("The indexes are", topics_selected)
-
-st.button("Start quiz", on_click=quiz)
+if st.session_state.show_topic_choice == True:
+    options = st.multiselect(
+        "What topics would you like to be quizzed on?",
+        (topics_list)
+    )
+    
+    topics_selected = []
+    for x in topics_list:
+        if x in options:
+            topics_selected.append(topics_list.index(x))
+    
+    
+    #st.write("You selected:", options)
+    #st.write("The indexes are", topics_selected)
+    
+    st.button("Start quiz", on_click=quiz)
 
 
 
