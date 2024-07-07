351244f9f35213c0921220b7da10f3a1151d653a
saanvikhetan <saanvikhetan@gmail.com>
2024-07-07 20:50:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,22 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+topics_list = ["Securities", "Securities-based derivatives contract", "Securities Industry Council"]
+
 options = st.multiselect(
     "What topics would you like to be quizzed on?",
-    (["Securities", "Securities-based derivatives contract", "Securities Industry Council"])
+    (topics_list)
 )
 
+topics_selected = []
+for x in topics_list:
+    if x in options:
+        topics_selected.append(topics_list.index(x))
+
+
 st.write("You selected:", options)
+st.write("The indexes are", topics_selected)
+
 
 
 
