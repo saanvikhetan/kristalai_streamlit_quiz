268bbf4da9b1f2cfcc62ceec458a20985aa4bfeb
saanvikhetan <saanvikhetan@gmail.com>
2024-07-07 21:45:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ def read_csv():
     st.session_state.question_bank = []
     for x in range(len(topics_list)):
         file_path = f"questions_{x}"
+        st.write("reading", file_path)
         df = pd.read_csv(file_path, sep='\t')
+        st.write("read " + df.shape[0] + " rows")
         st.session_state.question_bank.append(df.values.tolist())
     st.write(st.session_state.question_bank)
 
